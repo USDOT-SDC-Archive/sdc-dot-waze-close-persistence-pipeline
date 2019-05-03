a7843cc2016b5f6ad8a777205f4ba33477940c48
Santosh Karla <santosh.karla@gmail.com>
2019-05-03 11:15:02 -0400

    Add unit tests, ci definition and sonar properties

--- lambdas/persistence_close_statemachine_handler.py
+++ lambdas/persistence_close_statemachine_handler.py
@@ -4,43 +4,43 @@ import os
 from common.logger_utility import *
 from common.constants import *
 
-sns = boto3.client('sns')
+sns = boto3.client('sns', region_name='us-east-1')
 class ClosePipeline:
 
-    def __publish_message_to_sns(self, message):
+    def publish_message_to_sns(self, message):
         response = sns.publish(
             TargetArn=os.environ['BATCH_NOTIFICATION_SNS'],
             Message=json.dumps({'default': json.dumps(message)}),
             MessageStructure='json'
         )
 
-    def __delete_sqs_message(self,event, context):
+    def delete_sqs_message(self, event, context):
+        batchId = ""
         try:
             if "queueUrl" in event[0] and "batchId" in event[0]:
                 sqs = boto3.resource('sqs', region_name='us-east-1')
                 queueUrl = event[0]["queueUrl"]
                 receiptHandle = event[0]["receiptHandle"]
                 batchId = event[0]["batchId"]
-                is_historical = event[0]["is_historical"] == "true"
-                persistenceQueue = os.environ['SQS_PERSISTENCE_ORCHESTRATION_QUEUE_NAME']
-                if (is_historical):
-                    persistenceQueue = os.environ['SQS_PERSISTENCE_ORCHESTRATION_HIS_QUEUE_NAME']
-                queue = sqs.get_queue_by_name(QueueName=persistenceQueue)
+                # is_historical = event[0]["is_historical"] == "true"
+                # persistenceQueue = os.environ['SQS_PERSISTENCE_ORCHESTRATION_QUEUE_NAME']
+                # if (is_historical):
+                #     persistenceQueue = os.environ['SQS_PERSISTENCE_ORCHESTRATION_HIS_QUEUE_NAME']
                 txt=json.dumps(event[0])
                 if json.loads(txt).get("queueUrl") is not None:
-                    message = sqs.Message(queueUrl,receiptHandle)
+                    message = sqs.Message(queueUrl, receiptHandle)
                     message.delete()
                     LoggerUtility.logInfo("Message deleted from sqs for batchId {}".format(batchId))
-                    self.__publish_message_to_sns({"BatchId": batchId, "Status": "Persistence process completed"})
+                    self.publish_message_to_sns({"BatchId": batchId, "Status": "Persistence process completed"})
         except Exception as e:
             LoggerUtility.logError("Unable to delete sqs message for batchId {}".format(batchId))
             raise e
     
-    def __push_batchid_to_nightly_sqs_queue(self, event, context):
+    def push_batchid_to_nightly_sqs_queue(self, event, context):
         current_batch_id = ""
         try:
             if "batchId" in event[0]:
-                sqs = boto3.resource('sqs')
+                sqs = boto3.resource('sqs', region_name='us-east-1')
                 current_batch_id = event[0]["batchId"]
                 nightly_queue_name = os.environ["SQS_NIGHTLY_PERSISTENCE_QUEUE_NAME"]
                 nightly_batches_queue = sqs.get_queue_by_name(QueueName=nightly_queue_name)
@@ -48,11 +48,11 @@ class ClosePipeline:
                     'BatchId': current_batch_id
                 }), MessageGroupId="WazeNightlyPersistenceBatchesMessageGroup")
                 LoggerUtility.logInfo("Successfully pushed the message to nightly queue for batchid -"
-                                      " {}".format(current_batch_id))
+                                      " {} with response - {}".format(current_batch_id, response))
         except Exception as e:
-            LoggerUtility.logError("Unable to delete sqs message for batchId {}".format(current_batch_id))
+            LoggerUtility.logError("Unable to push sqs message to nightly queue for batchId {}".format(current_batch_id))
             raise e
     
     def close_pipeline(self, event, context):
-        self.__push_batchid_to_nightly_sqs_queue(event, context)
-        self.__delete_sqs_message(event, context)
+        self.push_batchid_to_nightly_sqs_queue(event, context)
+        self.delete_sqs_message(event, context)
---
+++ tests/close_persistence_pipeline_test.py
@@ -0,0 +1,88 @@
+from moto import mock_sns, mock_sqs, mock_events
+import sys
+import os
+import time
+import pytest
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from lambdas.persistence_close_statemachine_handler import *
+
+@mock_sns
+def test_publish_message_to_sns():
+    batchId = str(int(time.time()))
+    topic_name = "dev-dot-sdc-cloudwatch-alarms-notification-topic"
+    message = {"BatchId": batchId, "Status": "Persistence process completed"}
+    sns = boto3.client('sns', region_name='us-east-1')
+    response = sns.create_topic(Name=topic_name)
+    os.environ["BATCH_NOTIFICATION_SNS"] = response['TopicArn']
+    close_pipeline_obj = ClosePipeline()
+    close_pipeline_obj.publish_message_to_sns(message)
+    assert True
+
+
+@mock_sqs
+def test_delete_sqs_message():
+    with pytest.raises(Exception):
+        batchId = str(int(time.time()))
+        sqs = boto3.client('sqs', region_name='us-east-1')
+        response = sqs.create_queue(QueueName='dev-dot-sdc-waze-data-persistence-orchestration',
+                         Attributes={'FifoQueue': "false", 'DelaySeconds': "60", 'MaximumMessageSize': "262144",
+                                     'MessageRetentionPeriod': "1209600", 'VisibilityTimeout': "960"})
+        queue_url = response['QueueUrl']
+        queue_events = []
+        queue_event = dict()
+        queue_event["batchId"] = batchId
+        queue_event["queueUrl"] = queue_url
+        queue_event["receiptHandle"] = "test"
+        queue_events.append(queue_event)
+        print(queue_events)
+        close_pipeline_obj = ClosePipeline()
+        close_pipeline_obj.delete_sqs_message(queue_events, None)
+
+
+@mock_sqs
+def test_push_batchid_to_nightly_sqs_queue_raises_exception():
+    with pytest.raises(Exception):
+        sqs = boto3.client('sqs', region_name='us-east-1')
+        sqs.create_queue(QueueName='dev-dot-sdc-waze-data-nightly-elt.fifo',
+                                    Attributes={'FifoQueue': "true", 'DelaySeconds': "5", 'MaximumMessageSize': "262144",
+                                                'MessageRetentionPeriod': "1209600", 'VisibilityTimeout': "960",
+                                                'ContentBasedDeduplication': "true"})
+        generated_batch_id = str(int(time.time()))
+        queue_events = []
+        queue_event = dict()
+        queue_event["batchId"] = generated_batch_id
+        queue_events.append(queue_event)
+        close_pipeline_obj = ClosePipeline()
+        close_pipeline_obj.push_batchid_to_nightly_sqs_queue(queue_events, None)
+
+
+@mock_sqs
+def test_push_batchid_to_nightly_sqs_queue():
+    sqs = boto3.client('sqs', region_name='us-east-1')
+    response = sqs.create_queue(QueueName='dev-dot-sdc-waze-data-nightly-elt.fifo',
+                                Attributes={'FifoQueue': "true", 'DelaySeconds': "5", 'MaximumMessageSize': "262144",
+                                            'MessageRetentionPeriod': "1209600", 'VisibilityTimeout': "960",
+                                            'ContentBasedDeduplication': "true"})
+    queue_url = response['QueueUrl']
+
+    queue_name = queue_url[queue_url.rfind('/') + 1: len(queue_url)]
+    os.environ['SQS_NIGHTLY_PERSISTENCE_QUEUE_NAME'] = queue_name
+    generated_batch_id = str(int(time.time()))
+    queue_events = []
+    queue_event = dict()
+    queue_event["batchId"] = generated_batch_id
+    queue_events.append(queue_event)
+    close_pipeline_obj = ClosePipeline()
+    close_pipeline_obj.push_batchid_to_nightly_sqs_queue(queue_events, None)
+    assert True
+
+@mock_events
+def test_close_pipeline():
+    with pytest.raises(Exception):
+        close_pipeline_obj = ClosePipeline()
+        assert close_pipeline_obj.close_pipeline(None, None) is None
+
+
+
+
+
