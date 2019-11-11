fb1085417d9de3eb7ae4f8744bbcf7237103a2de
Hannah Kent <hkent@acrotron.com>
2019-11-11 13:43:10 -0700

    added context param back to lambda_handler

--- persistence_close_statemachine_handler_main.py
+++ persistence_close_statemachine_handler_main.py
@@ -2,7 +2,7 @@ from lambdas.persistence_close_statemachine_handler import *
 from common.logger_utility import *
 
 
-def lambda_handler(event, *args, **kwargs):
+def lambda_handler(event, context):
     LoggerUtility.set_level()
     close_pipeline_handle_event = ClosePipeline()
     close_pipeline_handle_event.close_pipeline(event)
